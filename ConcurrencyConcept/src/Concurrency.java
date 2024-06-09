d437abd0ed67b85cee909d9ff6393687b180b32a
Dexter <dexter.t.jones@outlook.com>
2024-06-08 21:55:21 -0600

    WIP - Concurrency

--- ConcurrencyConcept/src/Concurrency.java
+++ ConcurrencyConcept/src/Concurrency.java
@@ -1,2 +1,60 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Concurrency {
+    private static final Lock lock = new ReentrantLock();
+    private static final Condition cv = lock.newCondition(); // used to synchronize threads
+    private static boolean thread1Done = false;
+
+    public static void countUp() {
+        System.out.println("Count up:");
+
+        for (int i = 0; i <= 20; ++i) {
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) { //
+                e.printStackTrace();
+            }
+            lock.lock();
+            try {
+                System.out.println(i);
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        lock.lock(); // lock for thread1Done
+        try {
+            thread1Done = true;
+            cv.signal(); // Notifies second thread to start
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void countDown() {
+        lock.lock();
+        try {
+            while (!thread1Done) {
+                try {
+                    cv.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            System.out.println("Count down:");
+            for (int i = 20; i >= 0; --i) {
+                try {
+                    Thread.sleep(50); // pause for effect
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(i);
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
 }
--- ConcurrencyConcept/src/Main.java
+++ ConcurrencyConcept/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Thread t1 = new Thread(Concurrency::countUp);
+        Thread t2 = new Thread(Concurrency::countDown);
+
+        t1.start();
+        t2.start();
+
+        // join threads
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Blast off!");
     }
 }
